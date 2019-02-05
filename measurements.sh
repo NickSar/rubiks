7c8555cb4873175d69555f81a6df528ecdbf7d04
nikos sarris <sarris.nikos68@gmail.com>
2019-02-05 23:29:23 +0100

    measurements script

--- measurements.sh
+++ measurements.sh
@@ -4,13 +4,10 @@ SERVER_ADDRESS="fs0.das4.cs.vu.nl"
 IPL_ARGUMENTS="-Dibis.server.address=$SERVER_ADDRESS"
 module load prun
 
-for proc in 2
-  do
-    for cores in 1
+for cores in 1
     do
-      prun -v -$proc -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok1${proc}${cores}" -Dibis.pool.size=$((proc * cores)) rubiks.ipl.Rubiks >>t1_${proc}_${cores}.out 2>>t1_${proc}_${cores}.err
-      prun -v -$proc -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok2${proc}${cores}" -Dibis.pool.size=$((proc * cores)) rubiks.ipl.Rubiks >>t2_${proc}_${cores}.out 2>>t2_${proc}_${cores}.err
-      # prun -v -$proc -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok3${proc}${cores}" -Dibis.pool.size=$((proc * cores)) sokoban.ipl.Sokoban "tests/t3.txt" >>t3_${proc}_${cores}.out 2>>t3_${proc}_${cores}.err
-      # prun -v -$proc -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok4${proc}${cores}" -Dibis.pool.size=$((proc * cores)) sokoban.ipl.Sokoban "tests/t4.txt" >>t4_${proc}_${cores}.out 2>>t4_${proc}_${cores}.err
-  done;
+      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok1${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t1_${cores}.out 2>>t1_${cores}.err
+      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok2${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t2_${cores}.out 2>>t2_${cores}.err
+      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok3${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t3_${cores}.out 2>>t3_${cores}.err
+      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok4${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t4_${cores}.out 2>>t4_${cores}.err
 done
