1820a201f88b27ac03efd2ca1bfc999f64c820a4
nikos sarris <sarris.nikos68@gmail.com>
2019-02-05 23:34:34 +0100

    measurements script

--- measurements.sh
+++ measurements.sh
@@ -6,8 +6,5 @@ module load prun
 
 for cores in {1..16}
     do
-      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok1${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t1_${cores}.out 2>>t1_${cores}.err
-      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok2${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t2_${cores}.out 2>>t2_${cores}.err
-      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok3${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t3_${cores}.out 2>>t3_${cores}.err
-      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok4${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t4_${cores}.out 2>>t4_${cores}.err
+      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok1${cores}" -Dibis.pool.size=${cores} rubiks.ipl.Rubiks >>ex1_${cores}.out 2>>ex1_${cores}.err
 done
