1091b7264bccedc7c96483edaf94e12593178ff7
nikos sarris <sarris.nikos68@gmail.com>
2019-02-05 23:52:18 +0100

    size=4

--- measurements.sh
+++ measurements.sh
@@ -6,5 +6,5 @@ module load prun
 
 for cores in {2..16}
     do
-      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsoka1${cores}" -Dibis.pool.size=${cores} rubiks.ipl.Rubiks --size 4 >>ex2.out 2>>ex2.err
+      prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsokaro1${cores}" -Dibis.pool.size=${cores} rubiks.ipl.Rubiks --size 4 >>ex2.out 2>>ex2.err
 done
