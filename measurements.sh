5da418ca2346a276bd736831cfe35c58e2b09cc8
nikos sarris <sarris.nikos68@gmail.com>
2019-02-05 23:31:02 +0100

    measurements script

--- measurements.sh
+++ measurements.sh
@@ -4,7 +4,7 @@ SERVER_ADDRESS="fs0.das4.cs.vu.nl"
 IPL_ARGUMENTS="-Dibis.server.address=$SERVER_ADDRESS"
 module load prun
 
-for cores in 1
+for cores in {1..16}
     do
       prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok1${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t1_${cores}.out 2>>t1_${cores}.err
       prun -v -1 -np $cores bin/java-run $IPL_ARGUMENTS -Dibis.pool.name="tsok2${cores}" -Dibis.pool.size=$cores rubiks.ipl.Rubiks >>t2_${cores}.out 2>>t2_${cores}.err
